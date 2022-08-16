51a32de5610f9c1d12ac6e37bee0ceb327a5923d
Nan Yang <nanyang24@hotmail.com>
2022-08-16 17:59:05 +0800

    feat: update UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,17 @@
 import { ReactElement } from 'react';
-import { ActivateDeactivate } from './components/ActivateDeactivate';
 import { Short } from './components/Short';
-import { SectionDivider } from './components/SectionDivider';
-import { SignMessage } from './components/SignMessage';
-import { WalletStatus } from './components/WalletStatus';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Background from './components/Background';
 
 export function App(): ReactElement {
   return (
-    <Background>
-      {/* <ActivateDeactivate />
-      <SectionDivider />
-      <WalletStatus />
-      <SectionDivider /> */}
-      <Header />
-      {/* <SignMessage /> */}
-      <Short />
-      <Footer />
-    </Background>
+    <>
+      <Background>
+        <Header />
+        <Short />
+        <Footer />
+      </Background>
+    </>
   );
 }
--- frontend/src/components/Background.tsx
+++ frontend/src/components/Background.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import c from 'classnames';
+import { isMobile } from 'react-device-detect';
 
 import s from './Background.module.scss';
 
-function Background({ children }: {
-  children: React.ReactNode;
-}) {
+function Background({ children }: { children: React.ReactNode }) {
+  if (isMobile) {
+    return (
+      <div className={c(s.wrapper, s.mobile)}>
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%'
+          }}
+        >
+          This content is available only on PC
+        </div>
+      </div>
+    );
+  }
   return <div className={s.wrapper}>{children}</div>;
 }
 
